28df863ea7fb91d54fbc9bd9186ab834bea95a19
MehranSattar <54243372+MehranSattar@users.noreply.github.com>
2020-01-23 22:34:35 +0000

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;	
+
+namespace Fizzbuzz
+{
+    
+    public class Fizzbuzz
+    {
+        public static void Main(string[] args)
+        {
+		int i = 0;
+		while (true){
+            
+        i++;
+			
+        Console.WriteLine(FizzorBuzz.modulus(i));
+
+        
+		}
+		}
+        static class FizzorBuzz
+        {
+            public static dynamic modulus(int i)
+            {
+                if ((i % 5 == 0) && (i % 3 == 0))
+                {
+                    return "FizzBuzz";
+                }else if (i % 5 == 0){
+                    return "Buzz";
+                }else if (i % 3 == 0){
+                    return "Fizz";
+                }else {
+					return i;
+				}
+            }
+        }
+    }
+}
