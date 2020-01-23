8ad2bf9d796293d4129dc3afdaae88e6d5d83afe
MehranSattar <54243372+MehranSattar@users.noreply.github.com>
2020-01-23 22:37:36 +0000

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,22 +1,18 @@
-﻿using System;	
+using System;
 
 namespace Fizzbuzz
 {
-    
     public class Fizzbuzz
     {
         public static void Main(string[] args)
         {
-		int i = 0;
-		while (true){
-            
-        i++;
-			
-        Console.WriteLine(FizzorBuzz.modulus(i));
-
-        
-		}
-		}
+            int i = 0;
+            while (true)
+            {
+                i++;
+                Console.WriteLine(FizzorBuzz.modulus(i));
+            }
+        }
         static class FizzorBuzz
         {
             public static dynamic modulus(int i)
@@ -24,13 +20,19 @@ namespace Fizzbuzz
                 if ((i % 5 == 0) && (i % 3 == 0))
                 {
                     return "FizzBuzz";
-                }else if (i % 5 == 0){
+                }
+                else if (i % 5 == 0)
+                {
                     return "Buzz";
-                }else if (i % 3 == 0){
+                }
+                else if (i % 3 == 0)
+                {
                     return "Fizz";
-                }else {
-					return i;
-				}
+                }
+                else
+                {
+                    return i;
+                }
             }
         }
     }
